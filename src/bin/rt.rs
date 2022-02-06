b800ba38d6906c2b776ce024a5607d33379563da
Sahuagin <sahuagin@users.noreply.github.com>
2022-02-05 20:58:58 -0500

    Image looks same as tutorial. Chapter 11 finished. Next, Chapter 12: Where next?

--- src/bin/rt.rs
+++ src/bin/rt.rs
@@ -22,9 +22,11 @@ fn main() {
     //let radian: f64 = (std::f64::consts::PI/4.0).cos();
     let vfov: f64 = 20.0;
     let aspect: f64 = nx as f64 / ny as f64;
-    let look_from = vect!(-2.0, 2.0, 1.0);
+    let look_from = vect!(3.0, 3.0, 2.0);
     let look_at   = vect!(0.0, 0.0, -1.0);
     let vup       = vect!(0.0, 1.0, 0.0);
+    let dist_to_focus: f64 = (look_from-look_at).length();
+    let aperture = 2.0;
     
     let mut world = HitList::new();
     // chapter 10, camera
@@ -46,11 +48,14 @@ fn main() {
     // an interesting and easy trick with dielectric spheres is to note that if you use a 
     // negative radius, the geometry is unaffected but the surface normal
     // points inward, so it can be used as a bubble to make a hollow glass sphere.
-    //world.list.push(Box::new(Sphere::new(&Vec3::new(-1.0,0.0,-1.0), -0.45, Dielectric::new(1.5))));
+    world.list.push(Box::new(Sphere::new(&Vec3::new(-1.0,0.0,-1.0), -0.45, Dielectric::new(1.5))));
     let camera = Camera::new(look_from,
                         look_at,
                         vup,
-                        vfov, aspect);
+                        vfov,
+                        aspect,
+                        aperture,
+                        dist_to_focus);
 
     
     println!("P3\n{} {}\n255", nx, ny);
